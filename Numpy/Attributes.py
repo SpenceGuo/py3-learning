462504d328976d75500193d91a0c312223300bd5
SpenceGuo <tangguo1280@163.com>
2020-11-03 10:27:09 +0800

    Update Attributes.py

--- Numpy/Attributes.py
+++ Numpy/Attributes.py
@@ -1,15 +1,4 @@
 import numpy as np
-
-
-# 列表转化为矩阵 print(array) """ array([[1, 2, 3], [2, 3, 4]]) """
-array = np.array([
-    [1, 2, 3],
-    [2, 3, 4],
-    [4, 5, 6]
-])
-
-print(array)
-
-
+import pandas as pd
 
 
